50d0874af5512ca51c5ba4e4056d05e9f96078fe
arturocordovaaa <luisarturoc952@gmail.com>
2025-05-09 16:12:30 -0700

    agregue para que se guardara el json cada 10,000 revistas

--- scrapper/main.py
+++ scrapper/main.py
@@ -5,11 +5,13 @@ from scraper import obtener_info_revista
 from utils import cargar_json, guardar_json, necesita_actualizacion
 
 def main():
-    path_entrada = 'data/prueba1.json'
-    path_salida = 'data/detalles_revistas.json'
+    path_entrada = 'data/json/revistas.json'
+    path_salida = 'data/json/detalles_revistas.json'
 
     revistas = cargar_json(path_entrada)
     detalles = cargar_json(path_salida)
+
+    contador = 0
 
     for titulo, info in revistas.items():
         if titulo not in detalles or necesita_actualizacion(detalles[titulo].get("ultima_visita")):
@@ -19,11 +21,19 @@ def main():
                 datos["catalogos"] = info["catalogos"]
                 datos["ultima_visita"] = datetime.today().strftime("%Y-%m-%d")
                 detalles[titulo] = datos
+                contador += 1
+
+                # Guardado cada 50 revistas 
+                if contador % 50 == 0:
+                    guardar_json(detalles, path_salida)
+                    print(f" Progreso guardado: {contador} revistas procesadas")
+
         else:
             print(f"'{titulo}' ya está actualizado. Saltando.")
 
+    # Guardado final
     guardar_json(detalles, path_salida)
-    print(f"\nArchivo actualizado: {path_salida}")
+    print(f"\n Archivo final actualizado: {path_salida}")
 
 if __name__ == '__main__':
     main()
