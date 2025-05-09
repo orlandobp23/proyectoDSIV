5b2ca2b29aa1ada6453fd39016118f3ec962051d
angluisb <luis042k@hotmail.com>
2025-05-08 19:33:02 -0700

    ya funciona creo

--- data/csv_to_json.py
+++ data/csv_to_json.py
@@ -12,31 +12,30 @@ ruta_salida = "data/json/revistas.json"
 
 # leer areas
 for archivo in os.listdir(folder_areas):
-    area = archivo.replace(".csv", "").upper()
+    area = archivo.replace(".csv", "").split()[0].split(" ")[0].upper()
     ruta = os.path.join(folder_areas, archivo)
     df = pd.read_csv(ruta, encoding="latin1")
     
-
-for titulo in df['TITULO:']:
-    titulo_limpio = titulo.strip().lower()
-    if titulo_limpio not in revistas:
-        revistas[titulo_limpio] = {"areas":[],"catalogos":[]}
-    if area not in revistas[titulo_limpio]["areas"]:
-        revistas[titulo_limpio]["areas"].append(area)   
+    for titulo in df['TITULO:']:
+        titulo_limpio = titulo.strip().lower()
+        if titulo_limpio not in revistas:
+            revistas[titulo_limpio] = {"areas":[],"catalogos":[]}
+        if area not in revistas[titulo_limpio]["areas"]:
+            revistas[titulo_limpio]["areas"].append(area)   
 
 
 # leer catalogos
 for archivo in os.listdir(folder_catalogos):
-    catalogo = archivo.replace(".csv","").upper()
+    catalogo = archivo.replace(".csv", "").split()[0].split(" ")[0].upper()
     ruta = os.path.join(folder_catalogos, archivo)
     df = pd.read_csv(ruta, encoding="latin1")
     
-for titulo in df['TITULO:']:
-    titulo_limpio = titulo.strip().lower()
-    if titulo_limpio not in revistas:
-        revistas[titulo_limpio] = {"areas":[],"catalogos":[]}
-    if catalogo not in revistas[titulo_limpio]["catalogos"]:
-        revistas[titulo_limpio]["catalogos"].append(area)
+    for titulo in df['TITULO:']:
+        titulo_limpio = titulo.strip().lower()
+        if titulo_limpio not in revistas:
+            revistas[titulo_limpio] = {"areas":[],"catalogos":[]}
+        if catalogo not in revistas[titulo_limpio]["catalogos"]:
+            revistas[titulo_limpio]["catalogos"].append(catalogo)
 
 #guardar
 with open(ruta_salida, "w", encoding="utf-8") as f:
